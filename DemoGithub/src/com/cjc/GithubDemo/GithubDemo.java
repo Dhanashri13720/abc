05c1ef5b173bd83205ffa17e49665e2502c2f991
ADMIN <ADMIN@DESKTOP-0GO5QNF>
2024-09-26 10:31:29 +0530

    by abc

---
+++ DemoGithub/src/com/cjc/GithubDemo/GithubDemo.java
@@ -0,0 +1,18 @@
+package com.cjc.GithubDemo;
+
+public class GithubDemo {
+	public void m1()
+	{
+		System.out.println("This is m1 method");
+	}
+	
+	public void m2()
+	{
+		System.out.println("This is m2 method");
+	}
+	
+public static void main(String [] args)
+{
+	System.out.println("Welcome");
+}
+}
---
+++ DemoGithub/src/com/cjc/demo2/demo2.java
@@ -0,0 +1,12 @@
+package com.cjc.demo2;
+
+public class demo2 {
+	public static void main(String[]args)
+	{
+		System.out.println("Main method");
+	}
+public void m3()
+{
+	System.out.println("This is m3 method");
+}
+}
